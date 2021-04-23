e9bb8a7d810417b90597af8c7f90b78a2695f498
Devukth <78857657+Devukth@users.noreply.github.com>
2021-04-23 17:03:47 +0530

    Add files via upload

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
 function addUser() {
     localStorage.setItem('name', document.getElementById('name').value);
-    document.location = "chatterapp.html";
+    window.location.replace("chatterapp.html");
 }
